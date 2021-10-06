72debc1c4b103abcba1269429f478cb3318af7f6
Trace Brandt <trpbr92@gmail.com>
2021-10-06 10:21:16 -0500

    styling

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet, Button} from 'react-native';
 
+import Colors from '../../constants/Colors';
+
 const ProductItem = props => {
     return (
-        <View style={styles.product}>
-        <Image style={styles.image} source={{uri: props.image}} />
-        <Text style={styles.title}>{props.title}</Text>
-        <Text style={styles.price} >${props.price}</Text>
-          <View style={styles.action}>
-            <Button title="View Details" onPress={props.viewDetail} />
-            <Button title="Go To Cart" onPress={props.addToCart} />
-          </View>
+    <View style={styles.product}>
+        <View style={styles.imageContainer}>
+            <Image style={styles.image} source={{uri: props.image}} />
         </View>
+        <View style={styles.details}>
+            <Text style={styles.title}>{props.title}</Text>
+            <Text style={styles.price} >${props.price}</Text>
+        </View>
+          <View style={styles.action}>
+            <Button color={Colors.primary} title="View Details" onPress={props.viewDetail} />
+            <Button color={Colors.primary} title="Go To Cart" onPress={props.addToCart} />
+          </View>
+    </View>
             );
 };
 
@@ -27,9 +33,21 @@ const styles = StyleSheet.create({
         height: 300,
         margin: 20
     },
+    imageContainer:{
+        width: '100%',
+        height: '60%',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        overflow: 'hidden'
+    },
     image: {
         width: '100%',
-        height: '60%'
+        height: '100%'
+    },
+    details: {
+        alignItems: 'center',
+        height: '15%',
+        padding: 10
     },
     title: {
         fontSize: 18,
@@ -42,7 +60,9 @@ const styles = StyleSheet.create({
     action: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        height: '25%',
+        paddingHorizontal: 20
     }
 })
 
