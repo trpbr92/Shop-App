de74657e4ad4aa3be6fa2fe78d14f7bcf30a491b
Trace Brandt <trpbr92@gmail.com>
2021-10-06 07:41:04 -0500

    setting up store

---
+++ screens/shop/ProductOverview.js
@@ -0,0 +1,10 @@
+ import react from 'react';
+ import { FlatList } from 'react-native';
+
+ import React from 'react';
+ 
+ const ProductOverview = props => {
+     return <FlatList />
+ };
+ 
+ export default ProductOverview;
