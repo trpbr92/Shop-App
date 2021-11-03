c5d112aaff7973e53b71e212325a6b889091a5aa
Trace Brandt <trpbr92@gmail.com>
2021-11-03 09:54:12 -0500

    product details screen formatting

--- screens/shop/ProductDetails.js
+++ screens/shop/ProductDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {ScrollView, View, Text, Image, Button, StyleSheet} from 'react-native';
+import Colors from '../../constants/Colors';
 
 const ProductDetails = props => {
     const productId = props.navigation.getParam('productId');
@@ -10,9 +11,14 @@ const selectedProduct = useSelector(state =>
     );
 
     return (
- <View>
-     <Text>{selectedProduct.title }</Text>
- </View>
+        <ScrollView>
+            <Image style={styles.image} source={{uri: selectedProduct.imageUrl}}/>
+            <View style={styles.actions}>
+            <Button color ={Colors.primary} title="Add to Cart" onPress={() => {}}/>
+            </View>
+            <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+            <Text style={styles.description}>{selectedProduct.description}</Text>
+        </ScrollView>
     );
 };
 
@@ -23,7 +29,25 @@ return {
 };
 
 const styles = StyleSheet.create({
-    
+    image: {
+        width: '100%',
+        height: 300
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20
+    },
+    actions: {
+        marginVertical: 10,
+        alignItems: 'center'
+    }
 });
 
 export default ProductDetails;
